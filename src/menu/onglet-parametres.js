3038d11e5fb57e45e19d77ca40ed2411c502329a
viglino <jean-marc.viglino@ign.fr>
2025-06-06 10:57:06 +0200

    [UPD] custom mapzones

--- src/menu/onglet-parametres.js
+++ src/menu/onglet-parametres.js
@@ -1,8 +1,13 @@
 import story from '../story';
 import charte from 'mcutils/charte/macarte'
+import dialogImportFile from 'mcutils/dialog/dialogImportFile'
+import ol_ext_element from 'ol-ext/util/element'
+import {buffer as buffer_extent, isEmpty } from 'ol/extent'
 
 import '../../page/onglet-parametres.css'
 import html from '../../page/onglet-parametres.html';
+import dialog from 'mcutils/dialog/dialog';
+import notification from 'mcutils/dialog/notification';
 
 // New tab
 const ongletParametres = charte.addMenuTab('options', 'fi-configuration', 'Paramètres carte', html);
@@ -32,7 +37,38 @@ displayAttributes.forEach(v => {
 
 // Mapzone
 ongletParametres.querySelector('[data-attr="mapzone"]').addEventListener('change', e => {
-  story.getCarte().setMapZone(e.target.value, false)
+  // Get zone
+  const what = e.target.value;
+  if (what === 'custom') {
+    dialogImportFile(e => {
+      if (story.getCarte()) story.getCarte().setMapZone('custom', false)
+      let nb = 0
+      e.features.forEach((f, i) => {
+        let ext = f.getGeometry().getExtent()
+        if (ext[0]===ext[2] || ext[1] === ext[3]) {
+          ext = buffer_extent(ext, 0.1)
+        }
+        if (!isEmpty(ext)) {
+          nb++;
+          if (story.getCarte()) {
+            story.getCarte().getControl('mapzone').addZone({
+              title: f.get('name') || f.get('nom') || 'zone '+i,
+              extent: ext
+            })
+          }
+        }
+      })
+      notification.show('Chargement des zones : ' + nb + ' / ' + e.features.length)
+    }, {
+      title: 'Définir des zones',
+      className: 'mc-mapzone',
+      readCarte: false,
+      projection: 'EPSG:4326'
+    })
+    dialog.getContentElement().appendChild(ol_ext_element.create('I', { text: "Choisissez un fichier contenant des zones de focalisation." }))
+  } else {
+    if (story.getCarte()) story.getCarte().setMapZone(what, false)
+  }
 })
 // Layerswitcher
 ongletParametres.querySelector('[data-attr="switcherModel"]').addEventListener('change', (e) => {
